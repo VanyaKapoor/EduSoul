2d041bb1819a61bf93129f2c2c43adad5a413bcd
iMADi-ARCH <nandanaditya985@gmail.com>
2021-06-16 15:12:07 +0530

    Cards + nav fix

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
-function main(){
-
+function main() {
 }
 
+navbar = document.getElementById("nav");
+window.onscroll = function () {
+    if (document.body.scrollTop > window.innerHeight / 5 || document.documentElement.scrollTop > 50) {
+        navbar.style.width = "100%";
+        // navbar.style.position = "fixed";
+        document.getElementById("nav-bg").style.width = "100%";
+    } else {
+        // navbar.style.position = "sticky";
+        document.getElementById("nav-bg").style.width = "100px";
+    }
+};
 
 document.addEventListener('DOMContentLoaded', main);
