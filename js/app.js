b9afded2b262981f503303f410bb0594270f1539
ritesh423 <riteshsingh.py@gmail.com>
2022-08-27 22:29:07 +0530

    added a modal on the donate button that we are not accepting donations now.

--- js/app.js
+++ js/app.js
@@ -108,3 +108,17 @@ document.addEventListener("DOMContentLoaded", main);
 
 
 // Donation Js
+const modal = document.querySelector('.modal');
+const hideButton = document.querySelector('.modal #hideButton');
+const showButton = document.querySelector('#showButton');
+
+hideButton.addEventListener('click', hideModal);
+showButton.addEventListener('click', showModal);
+
+function hideModal() {
+    modal.id = 'hide';
+}
+
+function showModal() {
+    modal.id = 'show';
+}
